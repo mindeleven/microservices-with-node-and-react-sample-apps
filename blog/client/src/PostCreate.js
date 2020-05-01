962933bcf46056ae04fe08c71f7affa387899ed5
Jürgen Kober <mindeleven@posteo.de>
2020-05-01 13:07:31 +0200

    Implemented basic post create form for the blog client.

---
+++ blog/client/src/PostCreate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <form>
+        <div className="form-group">
+          <label>Title</label>
+          <input className="form-control" />
+          <button className="btn btn-primary">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
