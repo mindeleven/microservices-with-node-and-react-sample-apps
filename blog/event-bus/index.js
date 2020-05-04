36445599af52b6ff189bb2e41bff7c3027eace34
Jürgen Kober <mindeleven@posteo.de>
2020-05-04 13:47:43 +0200

    Adding some functionality to the event bus.

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -4,3 +4,17 @@ const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {
+  const event = res.body;
+
+  axios.post('http://localhost:4000/events', event);
+  axios.post('http://localhost:4001/events', event);
+  axios.post('http://localhost:4002/events', event);
+
+  res.send({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+  console.log('listening on 4005');
+});
