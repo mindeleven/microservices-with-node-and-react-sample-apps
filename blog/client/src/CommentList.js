11a7d570b150813e0c4866136b9563b7b1785233
Jürgen Kober <mindeleven@posteo.de>
2020-05-09 12:55:07 +0200

    Modifying the CommentList to make use of the different states.

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -3,9 +3,21 @@ import React from 'react';
 export default ({ comments }) => {
 
   const renderedComments = Object.values(comments).map(comment => {
+    if (comment.status === 'approved') {
+      comment = comment.content;
+    }
+
+    if (comment.status === 'pendig') {
+      comment = 'This comment is awaiting moderation.';
+    }
+
+    if (comment.status === 'rejected') {
+      comment = 'This comment has been rejected.';
+    }
+
     return (
       <li key={comment.id} >
-        {comment.content}
+        {comment}
       </li>
     )
   });
@@ -13,5 +25,5 @@ export default ({ comments }) => {
   return (
     <ul>{renderedComments}</ul>
   );
-  
+
 };
