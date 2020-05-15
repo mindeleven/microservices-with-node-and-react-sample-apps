04d352a36de2419dd4c5021a9023b02103e8ad22
Jürgen Kober <mindeleven@posteo.de>
2020-05-15 12:57:21 +0200

    Call for handleEvent was misplaced.

--- blog/query/index.js
+++ blog/query/index.js
@@ -57,7 +57,6 @@ app.listen(4002, async () => {
 
    for (let event of res.data) {
      console.log('Processing event:', event.type);
+     handleEvent(event.type, event.data);
    }
-
-   handleEvent(event.type, event.data);
 });
