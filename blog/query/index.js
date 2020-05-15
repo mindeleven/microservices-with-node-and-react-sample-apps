6613f5638792c2053da22fd09d252a359d3da217
Jürgen Kober <mindeleven@posteo.de>
2020-05-15 11:54:39 +0200

    Modifying the query service to retrieve all events from the event-bus after start..

--- blog/query/index.js
+++ blog/query/index.js
@@ -8,12 +8,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
+const handleEvent = (type, data) => {
 
   if (type === 'PostCreated') {
     const { id, title } = data;
@@ -40,10 +35,28 @@ app.post('/events', (req, res) => {
     comment.content = content;
   }
 
-  console.log(posts);
+};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
+
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('listening on 4002');
+
+  const res = await axios.get('http://localhost:4005/events');
+
+   for (let event of res.data) {
+     console.log('Processing event:', event.type);
+   }
+
+   handleEvent(event.type, event.data);
 });
